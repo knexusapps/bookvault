c2ca5e6654dd076dd2a0302bdbb9c37896a5d0a3
TejDeepM <ennotationclub2024@gmail.com>
2024-11-16 18:44:54 +0400

    trying

---
+++ script.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    databaseURL: "https://623177569529.firebaseio.com"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Storage
+const database = firebase.database();
